dd8eab4d4168912b249127a2186d0af43d8134ce
Matthew <m.zadrozny@gmail.com>
2014-01-16 13:05:17 -0500

    Problem 2

---
+++ PE2.py
@@ -0,0 +1,14 @@
+'''PE2: By considering the terms in the Fibonacci sequence 
+whose values do not etotalceed four million, find the sum of the even-valued terms.'''
+
+total = 0 
+
+prior, current = 1, 2
+
+while current <= 4000000:
+    if current % 2 == 0:
+        total += current
+ 
+    prior, current = current, prior + current
+
+print total      
