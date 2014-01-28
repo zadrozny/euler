c56f64738765ab1bfcc63c21a5a9423bf35c8ee9
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-01-28 11:52:51 -0500

    Update PE014.py

--- PE014.py
+++ PE014.py
@@ -11,7 +11,7 @@ def findLength(n):
 	length = 1 					 #For n=1, length=1
 	while n != 1: 
 		if n%2 == 0: n = n/2
-		else:  		 n = 3*n + 1
+		else:  	     n = 3*n + 1
 		length += 1
 
 	return length
