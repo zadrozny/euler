6117de4400cf57477db182c0d27aec00ad503e6b
Matthew <m.zadrozny@gmail.com>
2014-01-29 12:09:28 -0500

    Changed variable name, editted comments

--- PE015.py
+++ PE015.py
@@ -5,15 +5,16 @@ a 20x20 grid [from the top left to the bottom right corner]?
 
 width, height = 20, 20
 
-d = {}
+paths = {}
+
 for y in range(height + 1): 
     for x in range(width + 1): 
         if 0 in (x, y): 
-            #All coordinates on edge have one path:
-            d[(x, y)] = 1
+            #All coordinates on left / top perimiter have only one path:
+            paths[(x, y)] = 1
         else: 
             #incoming paths = paths from left + paths from above:
-            d[(x, y)] = d[(x - 1), y] + d[(x, y - 1)]
+            paths[(x, y)] = paths[(x - 1), y] + paths[(x, y - 1)]
 
 
-print d[(20, 20)]
+print paths[(20, 20)]
