85ee5e93a3ea36fd9aa78830fd2210d0e77b8764
Matthew <m.zadrozny@gmail.com>
2014-02-02 11:46:26 -0500

    Problem 19

---
+++ PE019.py
@@ -0,0 +1,40 @@
+"""
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+"""
+
+from collections import deque
+days = deque(["Tuesday", "Wednesday", "Thursday", "Friday", 
+			  "Saturday", "Sunday", "Monday"])
+
+month_names = {1: "January", 2: "February", 3: "March", 
+			   4: "April", 5: "May", 6: "June",
+			   7: "July", 8: "August", 9: "September",
+			   10: "October", 11: "November", 12: "December"}
+
+months = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 
+		  7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+sundays = 0
+
+for year in range(1901, 2001): 
+	if year % 4 == 0: months[2] = 29
+	else: months[2] = 28 
+	for month in range(1, 13):
+		for day_of_month in range(1, months[month] + 1): 
+		 	day_of_week = days.popleft()
+		 	days.append(day_of_week)
+		 	if day_of_month == 1 and day_of_week == "Sunday":
+		 		sundays += 1
+		 		#print day_of_week, month_names[month], day_of_month, year 
+
+print sundays 
