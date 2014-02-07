1f2cc7b7ea00a2efccd8b2e399c8ecf567f84806
Matthew <m.zadrozny@gmail.com>
2014-02-07 12:45:05 -0500

    Problem 25

---
+++ PE025.py
@@ -0,0 +1,27 @@
+#PE25.py
+"""
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+termLength = 1000
+
+
+#Solution one: more memory intesive, possibly clearer
+fibs = [1, 1]
+while True:
+	fibs.append(fibs[-1] + fibs[-2])
+	if len(str(fibs[-1])) == termLength:
+		print len(fibs)
+		break 
+
+
+#Solution two: less memory intensive, possibly more opaque
+prior, current = 1, 1
+terms = 2
+while True:
+	terms += 1
+	prior, current = current, prior + current
+	if len(str(current)) == termLength:
+		print terms
+		break
+		 
