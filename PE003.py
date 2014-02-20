28570ab7594d82dcda59f2c11e77d8a077fc2437
Matthew <m.zadrozny@gmail.com>
2014-02-20 15:24:58 -0500

    Changed variable name from x to current_prime

--- PE003.py
+++ PE003.py
@@ -7,14 +7,14 @@ What is the largest prime factor of the number 600851475143 ?
 
 n = 600851475143
 
-x = 3 					#Because n is odd, start with 3 and not 2
+current_prime = 3 					#Because n is odd, start with 3 and not 2
 
-candidate = None		#Potential largest prime factor
+candidate = None					#Potential largest prime factor
 
-while x <= n:
-	if n % x == 0: 
-		candidate = x
-        n = n / x 		#Update n to prevent redudancy
-	x += 2 				#Since all primes > 2 are odd
+while current_prime <= n:
+	if n % current_prime == 0: 
+		candidate = current_prime
+        n = n / current_prime 		#Update n to prevent redundancy
+	current_prime += 2 				#Since all primes > 2 are odd
 
 print candidate
