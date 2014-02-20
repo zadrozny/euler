eeb41b3da73642eb7ae7354b5a59d0493f675046
Matthew <m.zadrozny@gmail.com>
2014-02-20 15:23:15 -0500

    Cosmetic change

--- PE001.py
+++ PE001.py
@@ -1,4 +1,4 @@
-#PE 1: Find the sum of all the multiples of 3 or 5 below 1000.
+#PE001.py
+'''Find the sum of all the multiples of 3 or 5 below 1000.'''
 
 print sum(n for n in range(1000) if n%3==0 or n%5==0)
-
