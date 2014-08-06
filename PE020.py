549cb27194e0c908d6a14ccc32961a6b0b61d6a2
Matthew Zadrozny <MZ@localhost.localdomain>
2014-08-06 13:34:46 -0400

    Simplified the import statement

--- PE020.py
+++ PE020.py
@@ -1,5 +1,5 @@
 '''Find the sum of the digits in the number 100!'''
 
-import math
+from math import factorial
 
-print sum([int(digit) for digit in str(math.factorial(100))])
+print sum([int(digit) for digit in str(factorial(100))])
