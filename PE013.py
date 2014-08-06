de5058b8c0425be47d08a064ab8635dbcdceffb4
Matthew Zadrozny <MZ@localhost.localdomain>
2014-08-05 22:43:23 -0400

    Rewrote on one line

--- PE013.py
+++ PE013.py
@@ -1,11 +1,3 @@
 '''Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.'''
 
-path = r'ProjectEulerProblem13.txt'			#Number on PE site
-
-total = 0
-
-with open(path) as f: 
-	for line in f:
-		total += int(line.strip('\n'))
-
-print total 
+print str(sum(int(line) for line in number.split('\n')))[:10]
