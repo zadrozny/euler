9133e8990e4690fcc3040f26c7e110dd184661d9
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-10-16 12:09:07 -0400

    Removed line

--- PE048.py
+++ PE048.py
@@ -1,5 +1,4 @@
 '''Problem 48: Self powers The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.'''
 
-
 print str(sum([x**x for x in xrange(1, 1001)]))[-10:]
