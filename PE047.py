6e57b5ffce581218aaebf239a9f513dfa4c21752
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-10-21 12:42:45 -0400

    PE047.py

---
+++ PE047.py
@@ -0,0 +1,38 @@
+"""
+PE Problem #47
+==========================
+
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 * 7
+15 = 3 * 5
+
+The first three consecutive numbers to have three distinct prime factors
+are:
+
+644 = 2^2 * 7 * 23
+645 = 3 * 5 * 43
+646 = 2 * 17 * 19.
+
+Find the first four consecutive integers to have four distinct primes
+factors. What is the first of these numbers?
+"""
+
+#Runtime: 85 seconds
+
+# # Two of the numbers will be even and two will be odd
+# # One of the evens will be divisible by 4
+
+from euler_functions import factorize
+
+counter = 0
+i = 1
+while counter < 4:
+	if len(factorize(i)) == 4:
+		counter += 1
+	else: 
+		counter = 0
+	i+=1
+
+print i - 3 - 1 # Subract 3 because it is the 4th and 1 for i+=1
+
