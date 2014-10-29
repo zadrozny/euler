86a80873088ffa234681adc789b5394c4f98794a
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-10-29 11:41:02 -0400

    Changed range to xrange; made comment PEP 8 compliant

--- PE004.py
+++ PE004.py
@@ -6,15 +6,14 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers
 '''
 
-
 upper = 999
 lower = 100
-best = 0
+best  = 0
 
-for x in range(upper, lower, -1):
-	for y in range(x, lower, -1):
+for x in xrange(upper, lower, -1):
+	for y in xrange(x, lower, -1):
 		multiple = str(x*y)
-		if multiple == multiple[::-1]: 			#Check if palindrome
+		if multiple == multiple[::-1]: # Check if palindrome
 			if x*y > best: 
 				best = x*y 
 
