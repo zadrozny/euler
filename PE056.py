b209d9621a7a2c9c8daa04720a462002f0b346c7
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-10-31 18:30:09 -0400

    Changed range to xrange

--- PE056.py
+++ PE056.py
@@ -12,8 +12,8 @@ the maximum digital sum?
 
 largest = 0
 
-for a in range(1, 100):
-	for b in range(1, 100):
+for a in xrange(1, 100):
+	for b in xrange(1, 100):
 		total = sum(map(int, list(str(a**b))))
 		largest = max(largest, total)
 
