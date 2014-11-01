53c8b18b3ee2d5b855f423fb7fc0e3b64931ad95
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-11-01 15:10:45 -0400

    Changed variable name from counter to consecutive_integers more explicit

--- PE047.py
+++ PE047.py
@@ -23,16 +23,18 @@ factors. What is the first of these numbers?
 # # Two of the numbers will be even and two will be odd
 # # One of the evens will be divisible by 4
 
+
 from euler_functions import factorize
 
-counter = 0
+consecutive_integers = 0
 i = 1
-while counter < 4:
+while consecutive_integers < 4:
 	if len(factorize(i)) == 4:
-		counter += 1
+		consecutive_integers += 1
 	else: 
-		counter = 0
+		consecutive_integers = 0
 	i+=1
 
 print i - 3 - 1 # Subract 3 because it is the 4th and 1 for i+=1
+
 
--- PE049.py
+++ PE049.py
@@ -1,10 +1,29 @@
+"""
+Project Euler Problem #49
+==========================
+
+The arithmetic sequence, 1487, 4817, 8147, in which each of the terms
+increases by 3330, is unusual in two ways: (i) each of the three terms are
+prime, and, (ii) each of the 4-digit numbers are permutations of one
+another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit
+primes, exhibiting this property, but there is one other 4-digit
+increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this
+sequence?
+"""
+
 def check_primality(n):
 	'''Checks if odd number > 2 is prime'''
 	i = 3
 	while i <= n/i:
-		if n % i == 0: return False
+		if n % i == 0: 
+			return False
 		i+=2
 	return True
+
 
 def check_distances(primelist):
 	for x, prime_one in enumerate(primelist):
@@ -12,6 +31,7 @@ def check_distances(primelist):
 			for prime_three in primelist[y+1:]:
 				if prime_three - prime_two == prime_two - prime_one:
 					print str(prime_one)+str(prime_two)+str(prime_three)
+
 
 primes = {}
 for n in range(1001, 10000, 2):
@@ -23,3 +43,4 @@ for n in range(1001, 10000, 2):
 				check_distances(primes[key])
 		else:
 			primes[key] = [n]
+
