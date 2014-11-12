49aa10c843d7306ea9c2c850d588d69829e7a9bb
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-11-12 16:17:33 -0500

    Changed variable name from perm_key to permutation_key

--- PE062.py
+++ PE062.py
@@ -2,8 +2,8 @@
 Cubic permutations
 Problem 62
 
-The cube, 41063625 (3453), can be permuted to produce two other cubes: 
-56623104 (3843) and 66430125 (4053). In fact, 41063625 is the smallest 
+The cube, 41063625 (345^3), can be permuted to produce two other cubes: 
+56623104 (384^3) and 66430125 (4053). In fact, 41063625 is the smallest 
 cube which has exactly three permutations of its digits which are also cube.
 
 Find the smallest cube for which exactly five permutations of its digits are cube.
@@ -11,20 +11,20 @@ Find the smallest cube for which exactly five permutations of its digits are cub
 
 permutations = {}
 
-root = 345
+root = 345 # The cube's root must be larger than 345
 
 while True:
 	cube = root**3
 
-	perm_key = str(sorted(str(cube)))
+	permutation_key = str(sorted(str(cube)))
 
-	if perm_key not in permutations:
-		permutations[perm_key] = [cube]
+	if permutation_key not in permutations:
+		permutations[permutation_key] = [cube]
 	else:
-		permutations[perm_key].append(cube)
+		permutations[permutation_key].append(cube)
 
-		if len(permutations[perm_key]) == 5: # 'exactly five permutations...'
-			print permutations[perm_key][0]
+		if len(permutations[permutation_key]) == 5: # 'exactly five permutations'
+			print permutations[permutation_key][0]
 			break 
 
 	root+=1
