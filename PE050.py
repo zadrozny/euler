a6eabac6c096a5a58ce75b69c4ade3ac3d4804c0
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-11-16 21:19:15 -0500

    Main loop now uses generate_next_prime() instead of skipping evens. Code is still confusing and needs refactoring.

--- PE050.py
+++ PE050.py
@@ -16,18 +16,15 @@ Which prime, below one-million, can be written as the sum of the most
 consecutive primes?
 """
 
-from euler_functions import is_prime
+from euler_functions import generate_next_prime, is_prime
 
 #  Initialize with some primes to see the pattern
-#  primes = {prime: {number of preceding primes: sum of preceding primes}}
+#  primes = {prime: {previous n primes: sum of previous n primes, 
+# 					 previous n -1 primes: sum of previous n - 1 primes}
+# 			}
 
 primes = 	{2:  {0:  0}, 
-			 3:  {1:  2}, 
-			 5:  {2:  5,  1:  3}, 
-			 7:  {3: 10,  2:  8,  1:  5}, 
-			 11: {4: 17,  3: 15,  2: 12,  1:  7},
-			 13: {5: 28,  4: 26,  3: 23,  2: 18,  1: 11}
-			 }
+			 3:  {1:  2}}
 
 
 
@@ -38,40 +35,44 @@ def find(limit):
 	the sum of the most consecutive primes.
 	'''
 	
-	previous_prime = 13
-	candidate = 15      # previous_prime + 2
-	best_streak = 4     # 2 + 3 + 5 + 7 = 17
-	best_prime = 17     # ie, has the longest streak 
+	previous_prime = 2
+
+	best_streak = 0     # 2 + 3 = 5
+	best_prime = 0     # ie, has the longest streak 
+
+	g = generate_next_prime()
+
+	# Rewrite below when generate_next_prime has start and stop
+	for n in range(2): next(g) # Skip 2 and 3 (already in primes)
+	
+	candidate = next(g)
 
 	while candidate < limit: # 1000000:
+		
+		value = {prime + 1: primes[previous_prime][prime] + 
+					previous_prime for prime in primes[previous_prime]}														
+		
+		value[1] = previous_prime # Add preceding prime to the list
 
-		if is_prime(candidate):
+		candidate_streak = best_streak + 1
 
-			value = {prime + 1: primes[previous_prime][prime] + 
-						previous_prime for prime in primes[previous_prime]}																																																												
+		while candidate_streak in value:
+
+			embedded_candidate = value[candidate_streak]
+		
+			if embedded_candidate > limit:
+				return best_prime
+
+			elif is_prime(embedded_candidate):
+				best_streak = candidate_streak
+				best_prime = embedded_candidate
 			
-			value[1] = previous_prime # Add preceding prime to the list
+			candidate_streak += 1
+		
+		primes[candidate] = value # Append the primes list
+		previous_prime = candidate
 
-
-			candidate_streak = best_streak + 1
-
-			while candidate_streak in value:
-
-				embedded_candidate = value[candidate_streak]
-			
-				if embedded_candidate > limit:
-					return best_prime
-
-				elif is_prime(embedded_candidate):
-					best_streak = candidate_streak
-					best_prime = embedded_candidate
-				
-				candidate_streak += 1
-			
-			primes[candidate] = value # Append the primes list
-			previous_prime = candidate
-
-		candidate += 2
+		candidate = next(g) # Generate next prime
 
 	return best_prime
 
