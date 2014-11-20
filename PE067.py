0c3b4f4adde3ece1571c793322946e85a08ca819
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-11-19 19:22:09 -0500

    Refactored code, more elegant.

--- PE067.py
+++ PE067.py
@@ -4,14 +4,23 @@ import requests
 
 t = requests.get('http://projecteuler.net/project/resources/p067_triangle.txt').text
 
-t = list(reversed([map(int, line.split()) for line in t.split('\n')][:-1])) # Last row empty
+t = [map(int, line.split()) 
+		for line in t.split('\n')][:-1] # Remove empty last row
 
-for row_index, row in enumerate(t):
-	for number_index, number in enumerate(row):
-		try: 
-			larger = max(number, row[number_index+1]) 	# Choose larger of neighbors
-			t[row_index + 1][number_index] += larger 	# Add to row below (triangle is upside down)
-		except IndexError:
-			pass 
 
-print t[-1][0]
+first = t[0] # The top of the triangle
+previous_row = [0] * len(t[-1]) # Initialize a fake previous row of zeros.
+
+
+for row in reversed(t[1:]):
+
+	row = map(sum, zip(previous_row, row)) # First iteration with fake.
+	
+	# Keep larger of two neighbors in row:
+	row = [max(row[i], row[i+1]) for i, n in enumerate(row[:-1])]
+
+	previous_row = row # Update with real previous rows.
+
+
+print previous_row[0] + first[0] 
+
