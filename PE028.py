b7eb3f68fc5220963b0443c0ea91a280ba7b0db7
Matthew Zadrozny <m.zadrozny@gmail.com>
2014-12-01 12:52:00 -0500

    More reeadable version

--- PE028.py
+++ PE028.py
@@ -9,17 +9,17 @@ prior_square = 1 # The center of the spiral
 total = 1
 
 for n in range(3, 1003, 2): # Only consider odd squares; so step = 2
-	square = n**2
+    square = n**2
 
-	# Distance from, eg, 3^2 to next new corner, ie, 13
-	distance = (square - prior_square) / 4 
+    # Distance from, eg, 3^2 to next new corner, ie, 13
+    distance = (square - prior_square) / 4 
 
-	# Add each new corner:
-	total += (prior_square + 1*distance  + 
-			 (prior_square + 2*distance) + 
-			 (prior_square + 3*distance) + 
-			 (prior_square + 4*distance))
+    # Add each new corner:
+    total += (prior_square + 1*distance  + 
+             (prior_square + 2*distance) + 
+             (prior_square + 3*distance) + 
+             (prior_square + 4*distance))
 
-	prior_square = square
+    prior_square = square
 
 print total
