ff05a135a692cf03a9daf25a36b74574c65e5310
Matthew <m.zadrozny@gmail.com>
2015-12-13 19:35:10 -0500

    Slower, but much simpler solution

--- PE005.py
+++ PE005.py
@@ -1,26 +1,17 @@
 #PE5.py
-#Note: Could be optimized
+#Note: Could be optimized. Brute force but very readable. 
 '''
 What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20? 
 ''' 
 
-def find():											#Wrap in function to break out of loops
-	upper_divisor = 20 
-	lower_divisor = 11								#Skip dividing candidates by 1 - 10; implicit 
-	candidate 	  = upper_divisor 
-
-	while True:										#Check until found
-	    current_divisor = lower_divisor 			
-
-	    candidate += 20 							#Begin at 40 (could be optimized)
-	     
-	    while current_divisor < upper_divisor:
-	        if candidate % current_divisor == 0:          
-	            current_divisor += 1
-	        else: 
-	            break 								#Current divisor resets to lower_divisor
-	        if current_divisor == upper_divisor:
-	            return candidate 					#(And break out of inner and outer loop)
-
-print find()
+divisors = range(11, 20)
+candidate = 20
+while True:
+	for d in divisors:
+		if candidate % d != 0:
+			candidate += 20
+			break 
+	else:
+		print candidate
+		break 
