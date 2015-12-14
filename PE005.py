25671faa282d3656097b04c60ff9dc2d30a79662
Matthew <m.zadrozny@gmail.com>
2015-12-13 19:38:41 -0500

    PEP8

--- PE005.py
+++ PE005.py
@@ -5,13 +5,13 @@ What is the smallest positive number that is evenly divisible by all of the
 numbers from 1 to 20? 
 ''' 
 
-divisors = range(11, 20)
+divisors = range(11, 20) # Divisors 1 - 10 implicit
 candidate = 20
 while True:
-	for d in divisors:
-		if candidate % d != 0:
-			candidate += 20
-			break 
-	else:
-		print candidate
-		break 
+    for d in divisors:
+        if candidate % d != 0:
+            candidate += 20
+            break 
+    else:
+        print candidate
+        break 
