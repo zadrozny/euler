bdb820703e21ca754927c3fa9a1b560b7f116e9c
Matthew <m.zadrozny@gmail.com>
2015-12-18 20:14:41 -0500

    Vastly simplified

--- PE009.py
+++ PE009.py
@@ -1,29 +1,16 @@
-#PE9.py
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
 """
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
 
-def findTriplet():
-	a = 1
-	b = 1
+def find():
+    for a in range(1, 999):
+        for b in range(1, 1000 - a):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2:
+                return a*b*c
 
-	while a < 999:
-	    
-	    bplusc = 1000 - a   
-	      
-	    while b < bplusc:
-	        
-	        c = bplusc - b 
-	              
-	        if a*a + b*b == c*c:  
-	        	
-	            return '%s plus %s plus %s equals %s' % \
-	                   (str(a), str(b), str(c), str(a+b+c))
-
-	        b += 1
-	        
-	    b = 1    
-	    a += 1
-
-print findTriplet()
+print find()                
