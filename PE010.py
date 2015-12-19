d1ded047b1b9f85eeb15ed07af3e7332a2dd7418
Matthew <m.zadrozny@gmail.com>
2015-12-18 20:22:19 -0500

    Cosmetic

--- PE010.py
+++ PE010.py
@@ -3,13 +3,13 @@
 Find the sum of all the primes below two million. 
 """
 
-prime_list = [2] 	# Intialize with 2 to skip evens.
+prime_list = [2]                            # Intialize with 2 to skip evens
 
 for candidate in xrange(3, 2000000, 2): 		
     for prime in prime_list:			
-        if candidate % prime == 0:			# It's not prime.
+        if candidate % prime == 0:          # It's not prime
             break	
-        if prime > candidate/prime: 		# It is prime.
+        if prime > candidate/prime:         # It is prime
             prime_list.append(candidate) 	
             break				
 		
