69c572576976a355c4641093b8c4c3f053bdcaa6
Matthew <m.zadrozny@gmail.com>
2015-12-18 20:20:59 -0500

    Fixed indents

--- PE010.py
+++ PE010.py
@@ -3,16 +3,14 @@
 Find the sum of all the primes below two million. 
 """
 
-prime_list = [2] 					#Intialize with 2 to skip evens.
+prime_list = [2] 	# Intialize with 2 to skip evens.
 
-
-for n in xrange(3, 2000000, 2): 		
-	for p in prime_list:			
-		if n%p == 0:			 	#It's not prime.
-			break	
-		if p > n/p: 			 	#It is prime.
-			prime_list.append(n) 	
-			break				
-			
-
+for candidate in xrange(3, 2000000, 2): 		
+    for prime in prime_list:			
+        if candidate % prime == 0:			# It's not prime.
+            break	
+        if prime > candidate/prime: 		# It is prime.
+            prime_list.append(candidate) 	
+            break				
+		
 print sum(prime_list)
