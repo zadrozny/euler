7d7a47a7b8041ca9290273c4e64380bf80bda264
Matthew <m.zadrozny@gmail.com>
2015-12-21 13:16:45 -0500

    Removed old solution from file

--- PE001.py
+++ PE001.py
@@ -3,13 +3,3 @@
 '''Find the sum of all the multiples of 3 or 5 below 1000.'''
 
 print sum(n for n in xrange(1000) if n % 3 == 0 or n % 5 == 0)
-
-##############################################################
-
-total = 0
-
-for number in range(1000): 
-    if number % 3 == 0 or number % 5 == 0:
-        total = total + number 
-
-print total 
