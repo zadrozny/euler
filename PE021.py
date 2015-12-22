8cf100bd5c717b4d84b22f44628337578f4fa9d1
Matthew <m.zadrozny@gmail.com>
2015-12-22 12:14:04 -0500

    Removed superfluous list from sum(list(set x)), renamed variable and function

--- PE021.py
+++ PE021.py
@@ -2,29 +2,27 @@
 Evaluate the sum of all the amicable numbers under 10000.
 """
 
-def d(n): 
-	'''Returns the sum of the proper divisors of n.'''
-	divisors = [1] 			#Include 1 to optimize and exclude n.
+def sum_proper_divisors(n): 
+    '''Return sum of proper divisors of n'''
+    divisors = [1]            # Include 1 to optimize, exclude n
 
-	i = 2
-	while i <= n/i: 
-		if n % i == 0: 
-			divisors.extend([i, n/i])
-		i += 1
-
-	divisors = list(set(divisors)) #Remove duplicates from squares
-	return sum(divisors)
+    i = 2
+    while i <= n/i: 
+        if n % i == 0: 
+            divisors.extend([i, n/i])
+        i += 1
+     		
+    return sum(set(divisors)) # Remove duplicates from squares, then sum
 
 
 amicables = []
 
 for n in range(2, 10000): 
-	if n in amicables:
-		pass
-	else: 
-		sumOfProperDivisors = d(n)
-		if n == d(sumOfProperDivisors) and n != sumOfProperDivisors: 
-			amicables.extend([n, sumOfProperDivisors])
-
+    if n in amicables:
+        pass
+    else: 
+        total = sum_proper_divisors(n)
+        if n == sum_proper_divisors(total) and n != total: 
+            amicables.extend([n, total])
 
 print sum(amicables)
