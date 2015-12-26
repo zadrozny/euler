3fafc109310f723411ef65acbae6e9c66b3fb058
Matthew <m.zadrozny@gmail.com>
2015-12-26 12:32:38 -0500

    Cleaned comments

--- PE007.py
+++ PE007.py
@@ -8,11 +8,11 @@ prime_list = [2, 3]
 candidate = prime_list[-1]
 
 
-while len(prime_list)<10001:
-    candidate += 2							#Start at 5 and check only odds
+while len(prime_list) < 10001:
+    candidate += 2                    # Start at 5 and check only odds
     
-    for prime in prime_list[1:]:  			#Skip 2
-        if candidate % prime == 0:			#Not a prime
+    for prime in prime_list[1:]:      # Skip 2
+        if candidate % prime == 0:    # Not a prime
         	break
     else: 
     	prime_list.append(candidate)
