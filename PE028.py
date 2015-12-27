9ed4ba0e7069f41e75e2a0bc5ca2a9a11695d06f
Matthew <m.zadrozny@gmail.com>
2015-12-27 16:04:33 -0500

    Refactored, simplified

--- PE028.py
+++ PE028.py
@@ -4,22 +4,15 @@ What is the sum of the numbers on the diagonals in
 a 1001 by 1001 spiral...?
 '''
 
+total = 1 # The center of the spiral
 
-prior_square = 1 # The center of the spiral
-total = 1
-
-for n in range(3, 1003, 2): # Only consider odd squares; so step = 2
-    square = n**2
-
-    # Distance from, eg, 3^2 to next new corner, ie, 13
-    distance = (square - prior_square) / 4 
-
-    # Add each new corner:
-    total += (prior_square + 1*distance  + 
-             (prior_square + 2*distance) + 
-             (prior_square + 3*distance) + 
-             (prior_square + 4*distance))
-
-    prior_square = square
+# Start is 3 because we already have spiral center
+# Stop is the dimension of the square: 1001
+# Step is 2 because the 1001 is odd
+for i in range(3, 1001 + 1, 2):
+	# Start is the upper right corner
+	# Stop is the lower right corner
+	# Step is negative because we're spirling backwards
+    total += sum(range(i**2, i**2 - ((i - 1)*4), -1*(i - 1)))
 
 print total
