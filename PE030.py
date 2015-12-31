1923a4691823ec447120e3c4e751842084f52840
Matthew <m.zadrozny@gmail.com>
2015-12-31 11:21:58 -0500

    Added code to determine upper bound and comments

--- PE030.py
+++ PE030.py
@@ -1,12 +1,21 @@
 #PE30.py
 
-def check(n):
-	'''Returns True if a number is the sum of the fifth power of its digits'''
-	if n == sum(int(x)**5 for x in str(n)):
-		return True
-	else: 
-		return False
+# First we establish an upper bound for n
+# concatenating 9's until the concatenated number is 
+# larger than the sum of the fifth powers of its digits
 
-#The sum of all the numbers that can be written as 
-#the sum of fifth powers of their digits.
-print sum(n for n in range(2, 645705) if check(n))
+n = 9 
+while True:
+	bound = sum(9**5 for char in str(n))
+	if n > bound: 
+		break 
+	n = int(str(n)+str(9)) # Concatenate
+
+
+# Then we find the sum of all the numbers that can be 
+# written as the sum of fifth powers of their digits
+# (As 1 = 1^4 is not a sum it is not included.)
+
+print sum(n for n in range(2, bound) 
+	         if n == sum(int(char)**5 for char in str(n))) 
+
