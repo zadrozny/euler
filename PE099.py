ad35ef949170fee1022a1100e931604c541b0497
Matthew <m.zadrozny@gmail.com>
2015-12-31 19:08:16 -0500

    Problem 99, first stab (solution correct)

---
+++ PE099.py
@@ -0,0 +1,30 @@
+"""
+Project Euler Problem 99
+========================
+
+Comparing two numbers written in index form like 2^11 and 3^7 is not
+difficult, as any calculator would confirm that 2^11 = 2048 < 3^7 = 2187.
+
+However, confirming that 632382^518061 > 519432^525806 would be much more
+difficult, as both numbers contain over three million digits.
+
+Using base_exp.txt, a 22K text file containing one thousand lines with a
+base/exponent pair on each line, determine which line number has the
+greatest numerical value.
+
+NOTE: The first two lines in the file represent the numbers in the example
+given above.
+"""
+from math import log
+import requests
+
+r = requests.get('https://projecteuler.net/project/resources/p099_base_exp.txt').text
+
+
+# Create a list consisting of: line number (from 1 on), base, and exponent
+f = [[i]+[int(n) for n in l.split(',')] for i, l in enumerate(r.split('\n'), 1)]
+
+# Multiply the log of the base by the exponent for each pair
+# Sort the pairs by the size of the result
+# Get the the last line-base-exponent
+print sorted(f, key=lambda t: t[2]*log(t[1], 10))[-1] 
