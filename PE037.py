01e171b7b965c5cec211636f427f946797107a83
Matthew <m.zadrozny@gmail.com>
2016-01-05 15:32:36 -0500

    Removed isprime function and replaced with is_prime from euler_functions; added comment

--- PE037.py
+++ PE037.py
@@ -13,41 +13,19 @@ to right and right to left.
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
 
-
-def isprime(candidate):
-    '''Returns True if prime and False if not'''
-
-    if candidate == 1:
-        return False 
-
-    if candidate == 2:
-        return True
-    
-    if candidate == 3:
-        return True
-
-    if candidate % 2 == 0:
-        return False
-
-    divisor = 3
-    while divisor <= candidate / divisor:
-        if candidate % divisor == 0:
-            return False
-        divisor += 2
-    return True 
-
+from euler_functions import is_prime
 
 primes = []
+candidate = 23 # First truncatable prime
 
-candidate = 23
 while len(primes) < 11: 
-    if isprime(candidate):
+    if is_prime(candidate):
         s = str(candidate)
         truncations = [s[i:] for i in range(1, len(s))] + \
                         [s[:-i] for i in range(1, len(s))]
 
         for truncation in truncations:
-            if isprime(int(truncation)) == False:
+            if is_prime(int(truncation)) == False:
                 break 
         else: 
             primes.append(candidate)
