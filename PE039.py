138e154b410f1b8ce9ac99085a785adeaee5ac94
Matthew <m.zadrozny@gmail.com>
2016-01-09 23:04:23 -0500

    Replaced 1000 with p

--- PE039.py
+++ PE039.py
@@ -14,7 +14,7 @@ p = 1000                                  # p <= 1000
 for a in range(1, p+1-2):                 # Offset by 1, subtract 2 for b & c
     for b in range(a+1, p - a): 
         c = sqrt(a**2 + b**2) 
-        if c == int(c) and a+b+c <= 1000: # Is c an integer?
+        if c == int(c) and a+b+c <= p:    # Is c an integer?
             cnt[a+b+int(c)] += 1          # If yes, increment given p
 
 print cnt.most_common(1)[0][0]
