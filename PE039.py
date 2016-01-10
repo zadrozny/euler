75208c0916a4b2f4327e68dd722b5b4ec075f761
Matthew <m.zadrozny@gmail.com>
2016-01-09 20:53:22 -0500

    Still an ugly brute force solution, but slightly cleaned up. Fixed indents, removed useless variable.

--- PE039.py
+++ PE039.py
@@ -4,24 +4,23 @@ solutions for p = 120. {20,48,52}, {24,45,51}, {30,40,50} For which value of p <
 1000, is the number of solutions maximised?'''
 
 
-limit 		   = 10
 best_p 		   =  0
 solutions_best =  0
 
 
-for p in range(3, 1001):
+for perimeter in range(12, 1001): # Start with 3, 4, 5 triangle
 	
-	solutions_current = 0
+    solutions_current = 0
 
-	for a in range(1, p):
-		for b in range(a, p - a):
-			c = p - a - b
-			if a**2 + b**2 == c**2:
-				solutions_current += 1
+    for a in range(1, perimeter/2):
+        for b in range(a, perimeter-a):
+            c = perimeter - a - b
+            if a**2 + b**2 == c**2:
+                solutions_current += 1
 
-	if solutions_current > solutions_best:
-		solutions_best = solutions_current
-		best_p = p
+    if solutions_current > solutions_best:
+        solutions_best = solutions_current
+        best_p = perimeter
 
 
 print best_p
