45fd17e78b3447196ba24663554b0844918fa017
Matthew <m.zadrozny@gmail.com>
2016-01-09 23:06:26 -0500

    Edited comments

--- PE039.py
+++ PE039.py
@@ -7,7 +7,7 @@ solutions for p = 120. {20,48,52}, {24,45,51}, {30,40,50} For which value of p <
 from collections import Counter
 from math import sqrt
 
-cnt = Counter()                           # Perimeters and their counts
+cnt = Counter()                           # p's and their counts
 
 p = 1000                                  # p <= 1000
 
@@ -15,6 +15,6 @@ for a in range(1, p+1-2):                 # Offset by 1, subtract 2 for b & c
     for b in range(a+1, p - a): 
         c = sqrt(a**2 + b**2) 
         if c == int(c) and a+b+c <= p:    # Is c an integer?
-            cnt[a+b+int(c)] += 1          # If yes, increment given p
+            cnt[a+b+int(c)] += 1          # If yes, increment given (candidate) p
 
 print cnt.most_common(1)[0][0]
