8b1fbb14405ad539b8ab79dc8da6c33a523e80fa
Matthew <m.zadrozny@gmail.com>
2016-01-09 21:16:15 -0500

    Added if a**2 + b**2 > c**2: break, halving running time

--- PE039.py
+++ PE039.py
@@ -4,23 +4,24 @@ solutions for p = 120. {20,48,52}, {24,45,51}, {30,40,50} For which value of p <
 1000, is the number of solutions maximised?'''
 
 
-best_p 		   =  0
-solutions_best =  0
-
+best_perimeter = 0
+solutions_best = 0
 
 for perimeter in range(12, 1001): # Start with 3, 4, 5 triangle
 	
     solutions_current = 0
-
-    for a in range(1, perimeter/2):
-        for b in range(a, perimeter-a):
+    for a in range(1, perimeter/2 + 1): 
+        for b in range(a+1, perimeter-a):
             c = perimeter - a - b
             if a**2 + b**2 == c**2:
                 solutions_current += 1
 
+            if a**2 + b**2 > c**2: 
+            	break 
+
     if solutions_current > solutions_best:
         solutions_best = solutions_current
-        best_p = perimeter
+        best_perimeter = perimeter
 
 
-print best_p
+print best_perimeter
