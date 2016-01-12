95db7a48e2d3cea4804c3aaf0cab90f16da81a82
Matthew <m.zadrozny@gmail.com>
2016-01-12 09:58:50 -0500

    Fixed indents. Still needs refactoring: too slow

--- PE047.py
+++ PE047.py
@@ -18,10 +18,10 @@ Find the first four consecutive integers to have four distinct primes
 factors. What is the first of these numbers?
 """
 
-#Runtime: 85 seconds
+# Runtime: 85s
 
-# # Two of the numbers will be even and two will be odd
-# # One of the evens will be divisible by 4
+# Two of the numbers will be even and two will be odd
+# One of the evens will be divisible by 4
 
 
 from euler_functions import factorize
@@ -29,11 +29,11 @@ from euler_functions import factorize
 consecutive_integers = 0
 i = 1
 while consecutive_integers < 4:
-	if len(factorize(i)) == 4:
-		consecutive_integers += 1
-	else: 
-		consecutive_integers = 0
-	i+=1
+    if len(factorize(i)) == 4:
+        consecutive_integers += 1
+    else: 
+        consecutive_integers = 0
+    i+=1
 
 print i - 3 - 1 # Subract 3 because it is the 4th and 1 for i+=1
 
