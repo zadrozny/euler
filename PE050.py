1f06a2126567b4ef4004ba18d5184c2b24bfd3eb
Matthew <m.zadrozny@gmail.com>
2016-01-13 09:08:05 -0500

    Fixed indents. Needs refactoring.

--- PE050.py
+++ PE050.py
@@ -20,59 +20,58 @@ from euler_functions import generate_next_prime, is_prime
 
 #  Initialize with some primes to see the pattern:
 #  primes = {prime: {previous n primes: sum of previous n primes, 
-# 					 previous n -1 primes: sum of previous n - 1 primes}
-# 			}
+# 					 previous n -1 primes: sum of previous n - 1 primes}}
 
 primes = 	{2:  {0:  0}, 
-			 3:  {1:  2}}
+             3:  {1:  2}}
 
 
 
 def find(limit):
-	'''
-	Find prime below limit that can be written as
-	the sum of the most consecutive primes.
-	'''
+    '''
+    Find prime below limit that can be written as
+    the sum of the most consecutive primes.
+    '''
 	
-	previous_prime = 2
+    previous_prime = 2
 
-	best_streak = 0    # 2 + 3 = 5
-	best_prime = 0     # ie, has the longest streak 
+    best_streak = 0    # 2 + 3 = 5
+    best_prime = 0     # ie, has the longest streak 
 
-	g = generate_next_prime(start=5) 
+    g = generate_next_prime(start=5) 
 
-	candidate = next(g)
+    candidate = next(g)
 
-	while candidate < limit: # 1000000:
+    while candidate < limit: # 1000000:
 		
-		value = {prime + 1: primes[previous_prime][prime] + 
-					previous_prime for prime in primes[previous_prime]}														
+        value = {prime + 1: primes[previous_prime][prime] + 
+                 previous_prime for prime in primes[previous_prime]}														
 		
-		value[1] = previous_prime # Add preceding prime to the list
+        value[1] = previous_prime # Add preceding prime to the list
 
-		candidate_streak = best_streak + 1
+        candidate_streak = best_streak + 1
 
-		while candidate_streak in value:
+        while candidate_streak in value:
 
-			embedded_candidate = value[candidate_streak]
+            embedded_candidate = value[candidate_streak]
 		
-			if embedded_candidate > limit:
-				return best_prime
-
-			elif is_prime(embedded_candidate):
-				best_streak = candidate_streak
-				best_prime = embedded_candidate
+            if embedded_candidate > limit:
+                return best_prime
+            elif is_prime(embedded_candidate):
+                best_streak = candidate_streak
+                best_prime = embedded_candidate
 			
-			candidate_streak += 1
+            candidate_streak += 1
 		
-		primes[candidate] = value # Append the primes list
-		previous_prime = candidate
+        primes[candidate] = value # Append the primes list
+        previous_prime = candidate
 
-		candidate = next(g) # Generate next prime
+        candidate = next(g) # Generate next prime
 
-	return best_prime
+    return best_prime
 
 
 limit = 1000000	# "...which prime, below one million..."
 
 print find(limit)
+
