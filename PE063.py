0364ff5eb3e2bfb33d213dbbe7723291f8065b05
Matthew <m.zadrozny@gmail.com>
2016-01-15 10:46:19 -0500

    Fixed indents

--- PE063.py
+++ PE063.py
@@ -10,9 +10,9 @@ How many n-digit positive integers exist which are also an nth power?
 
 x = 0
 for p in range(1, 22):
-	for n in range(1, 10):
-		if len(str(n**p)) == p:
-			# print p, len(str(n**p)), n**p
-			x+=1
+    for n in range(1, 10):
+        if len(str(n**p)) == p:
+            # print p, len(str(n**p)), n**p
+            x+=1
 
 print x 
