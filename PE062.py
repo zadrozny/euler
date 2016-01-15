7903f4f17057acce6e1d48cc8065856c21606875
Matthew <m.zadrozny@gmail.com>
2016-01-15 10:44:51 -0500

    Fixed indents

--- PE062.py
+++ PE062.py
@@ -14,18 +14,18 @@ permutations = {}
 root = 345 # The cube's root must be larger than 345
 
 while True:
-	cube = root**3
+    cube = root**3
 
-	permutation_key = str(sorted(str(cube)))
+    permutation_key = str(sorted(str(cube)))
 
-	if permutation_key not in permutations:
-		permutations[permutation_key] = [cube]
-	else:
-		permutations[permutation_key].append(cube)
+    if permutation_key not in permutations:
+        permutations[permutation_key] = [cube]
+    else:
+        permutations[permutation_key].append(cube)
 
-		if len(permutations[permutation_key]) == 5: # 'exactly five permutations'
-			print permutations[permutation_key][0]
-			break 
+        if len(permutations[permutation_key]) == 5: 
+            print permutations[permutation_key][0]
+            break 
 
-	root+=1
+    root+=1
 
