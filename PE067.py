75175e81797b461b97ce89e20bc81c3e8c0be8ff
Matthew <m.zadrozny@gmail.com>
2016-01-15 10:49:17 -0500

    Fixed indents, edited and aligned comments

--- PE067.py
+++ PE067.py
@@ -1,26 +1,25 @@
 # PE067.py
 
-import requests
+from requests import get
 
-t = requests.get('http://projecteuler.net/project/resources/p067_triangle.txt').text
+t = get('http://projecteuler.net/project/resources/p067_triangle.txt').text
 
 t = [map(int, line.split()) 
-		for line in t.split('\n')][:-1] # Remove empty last row
+        for line in t.split('\n')][:-1] # Remove empty last row
 
 
-first = t[0] # The top of the triangle
-previous_row = [0] * len(t[-1]) # Initialize a fake previous row of zeros.
+first = t[0]                            # The top of the triangle
+previous_row = [0] * len(t[-1])         # Initialize a fake zeros.
 
 
 for row in reversed(t[1:]):
 
-	row = map(sum, zip(previous_row, row)) # First iteration with fake.
+    row = map(sum, zip(previous_row, row)) # First iteration with fake.
 	
-	# Keep larger of two neighbors in row:
-	row = [max(row[i], row[i+1]) for i, n in enumerate(row[:-1])]
+    # Keep larger of two neighbors in row:
+    row = [max(row[i], row[i+1]) for i, n in enumerate(row[:-1])]
 
-	previous_row = row # Update with real previous rows.
+    previous_row = row # Update with real previous rows.
 
 
 print previous_row[0] + first[0] 
-
